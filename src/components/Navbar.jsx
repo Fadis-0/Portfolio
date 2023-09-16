b90b0ff5c118b043dff77c981ec9331cd86712b0
Fadis-0 <ayadfadi10@gmail.com>
2023-09-16 11:31:01 +0000

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 	const normalLink = 'text-gray-400 hover:text-gray-500 transition-all ease text-lg';
 	
 	return (
-		<header className="bg-main-bg px-6 py-2 lg:px-16 lg:py-4 fixed top-0 w-full z-40">
+		<header className="bg-main-bg px-6 py-2 lg:px-16 lg:py-6 fixed top-0 w-full z-40">
 			<div className="max-w-7xl mx-auto flex">
 				<div className="text-md lg:text-lg lg:hidden font-semibold py-2">
 					Fadi Ayad
@@ -32,11 +32,14 @@ const Navbar = () => {
 							</p>
 						</NavLink>
 
+						<NavLink to="/contact" className={({ isActive }) => isActive ? activeLink : normalLink }>
+							<p className=" ">
+								Contact
+							</p>
+						</NavLink>
+
 
 						
-					</div>
-					<div className="ml-20 cursor-pointer transition-all ease rounded-sm bg-half-transparent text-gray-100 hover:bg-main-bg  hover:text-gray-800 py-2.5 px-6 flex items-center justify-center border-2 border-gray-700 font-semibold">
-						Contact
 					</div>
 
 				</div>
